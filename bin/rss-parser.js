e88fc52bfc4c49b0f32213483485c43cb4abb691
S.J <solovyev833@gmail.com>
2020-11-05 16:26:56 +0300

    Implement xml RSS parser

--- bin/rss-parser.js
+++ bin/rss-parser.js
@@ -1,3 +1,33 @@
-export default (data) => {
-  console.log(data);
+import axios from 'axios';
+import _ from 'lodash';
+
+const parse = (xml) => {
+  const id = _.uniqueId();
+  const parser = new DOMParser();
+  const document = parser.parseFromString(xml, 'application/xml');
+  const channelData = document.querySelector('channel');
+  const [channelTitle, channelDescription, channelLink] = Array.from(channelData.childNodes);
+  const data = {
+    id,
+    title: channelTitle.textContent,
+    description: channelDescription.textContent,
+    link: channelLink.textContent,
+  };
+  const items = Array.from(channelData.querySelectorAll('item'));
+  const contents = items.map((item) => {
+    const [title, description, link, , creator, pubDate] = Array.from(item.childNodes);
+    return {
+      id,
+      title: title.textContent,
+      description: description.textContent,
+      link: link.textContent,
+      creator: creator.textContent,
+      pubDate: pubDate.textContent,
+    };
+  });
+  return [data, contents];
 };
+
+export default (link) => axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(link)}`)
+  .then(({ data }) => parse(data.contents))
+  .catch((error) => error);

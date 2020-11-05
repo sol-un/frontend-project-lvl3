c87a1a2c423a275a38f27c315beecbb281609e92
S.J <solovyev833@gmail.com>
2020-11-05 16:27:17 +0300

    Implement xml RSS parser test

--- __tests__/rss-parser.test.js
+++ __tests__/rss-parser.test.js
@@ -1,7 +1,8 @@
 import rssParser from '../bin/rss-parser.js';
 
 describe('RSS Parser', () => {
-  it('tests should run', () => {
-    expect(rssParser()).toBe(undefined);
+  it('data should download', async () => {
+    const [, contents] = await rssParser('http://lorem-rss.herokuapp.com/feed');
+    expect(contents.length).toBe(10);
   });
 });

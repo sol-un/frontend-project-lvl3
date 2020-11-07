1ec9dc8a7cdfffb12fcf4b9a1b29a091f09d9649
S.J <solovyev833@gmail.com>
2020-11-07 10:58:29 +0300

    Improve the robustness of data extracting in the parser algorithm

--- bin/parser.js
+++ bin/parser.js
@@ -1,33 +1,36 @@
 import axios from 'axios';
 import _ from 'lodash';
 
+const retrieveText = (element) => (tagName) => (element.querySelector(tagName)
+  ? element.querySelector(tagName).textContent
+  : null);
+
 const parse = (xml) => {
   const id = _.uniqueId();
   const parser = new DOMParser();
   const document = parser.parseFromString(xml, 'application/xml');
   const channel = document.querySelector('channel');
-  const [channelTitle, channelDescription, channelLink] = Array.from(channel.childNodes);
+  const retrieveFromChannel = retrieveText(channel);
   const data = {
     id,
-    title: channelTitle.textContent,
-    description: channelDescription.textContent,
-    link: channelLink.textContent,
+    title: retrieveFromChannel('title'),
+    description: retrieveFromChannel('description'),
+    link: retrieveFromChannel('link'),
   };
   const items = Array.from(channel.querySelectorAll('item'));
   const contents = items.map((item) => {
-    const [title, description, link, , creator, pubDate] = Array.from(item.childNodes);
+    const retrieveFromItem = retrieveText(item);
     return {
       id,
-      title: title.textContent,
-      description: description.textContent,
-      link: link.textContent,
-      creator: creator.textContent,
-      pubDate: pubDate.textContent,
+      title: retrieveFromItem('title'),
+      description: retrieveFromItem('description'),
+      link: retrieveFromItem('link'),
+      creator: retrieveFromItem('creator'),
+      pubDate: retrieveFromItem('pubDate'),
     };
   });
   return [data, contents];
 };
 
 export default (link) => axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(link)}`)
-  .then(({ data }) => parse(data.contents))
-  .catch((error) => error);
+  .then(({ data }) => parse(data.contents));

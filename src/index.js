dfa6792fb23b94dcbee5f82cf6bf31e983717159
S.J <solovyev833@gmail.com>
2020-11-07 11:01:39 +0300

    Rename the .css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './cover.css';
+import './styles.css';
 import app from '../bin/application.js';
 
 app();

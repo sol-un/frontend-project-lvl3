c09f0b0b1dbed61001314a1c355c30ce18411e75
Andrei Soloviov <solovyev833@gmail.com>
2021-01-20 08:30:53 +0300

    Name data-handler.js correctly, rectify tests

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -1,8 +1,8 @@
-import parser from '../src/parser.js';
+import process from '../src/data-handler.js';
 
 describe('Parser: normal operation', () => {
   it('Should handle test feed', async () => {
-    const [data, contents] = await parser('http://lorem-rss.herokuapp.com/feed');
+    const [data, contents] = await process('http://lorem-rss.herokuapp.com/feed');
     expect(data.title).toBe('Lorem ipsum feed for an interval of 1 minutes with 10 item(s)');
     expect(contents.length).toBe(10);
   });

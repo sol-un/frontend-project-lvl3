291eb4c8d191dbfe841db16ead41d1396bca452b
Andrei Soloviov <solovyev833@gmail.com>
2021-01-30 15:27:07 +0300

    Remove the github widget and all custom styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import addWidget from './widget.js';
 import app from './application.js';
 
-app().then(() => addWidget());
+app();

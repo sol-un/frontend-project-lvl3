2b5e02aea0e7d5c6bc032e6eb487621d6dffdd9b
Andrei Soloviov <solovyev833@gmail.com>
2021-03-04 17:31:36 +0300

    Change no-devtool value to false

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
-const devtool = mode === 'development' ? 'source-map' : null;
+const devtool = mode === 'development' ? 'source-map' : false;
 
 module.exports = {
   mode,
